f64cb4e39226c973526d5ba6d28b377dec856a45
Murali Annamneni <murali.annamneni@oracle.com>
2025-06-20 15:53:43 +0000

    Update script

--- test.js
+++
@@ -1,3 +0,0 @@
-// JavaScript file to please CodeQL scan
-
-console.log("Detected");
---
+++ test.ts
@@ -0,0 +1,6 @@
+// Simple TypeScript to please scanner
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
