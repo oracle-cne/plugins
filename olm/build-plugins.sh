f52496326bbc81993d5ecc349b21eb33fe99c0f1
Murali Annamneni <murali.annamneni@oracle.com>
2026-02-25 12:43:12 +0000

    Add patch files for app-catalog
    app-catalog patch files to rename service annotations
    
    Signed-off-by: Murali Annamneni <murali.annamneni@oracle.com>

--- olm/build-plugins.sh
+++ olm/build-plugins.sh
@@ -30,11 +30,13 @@ done
 declare -A plugins_map
 # map in the format 'plugin_name'='plugin-oracle_release_branch'
 plugins_map=(
-  ["app-catalog"]="53-add-build-data-for-app-catalog-080"
-  ["prometheus"]="57-add-build-data-for-prometheus-081"
+  ["app-catalog"]="oracle/release/app-catalog-0.8.0"
+  ["prometheus"]="oracle/release/prometheus-0.8.2"
   ["cert-manager"]="oracle/release/cert-manager-0.1.0"
   ["ai-assistant"]="oracle/release/ai-assistant-0.1.0"
 )
+
+cp ./buildrpm/*.patch /tmp/
 
 mkdir -p $PLUGINS_DIR
 current_branch=$(git name-rev --name-only HEAD)
@@ -43,6 +45,10 @@ for current_plugin in "${!plugins_map[@]}"; do
 
   git checkout -f ${plugins_map[$current_plugin]}
   pushd $current_plugin
+  if [[ "$current_plugin" == "app-catalog" ]];then
+    patch -p0 < /tmp/app-catalog-api-catalogs.patch
+    patch -p0 < /tmp/app-catalog-helpers-catalog.patch
+  fi
   npm install
   # Build the plugin
   npx @kinvolk/headlamp-plugin build .
@@ -55,4 +61,4 @@ for current_plugin in "${!plugins_map[@]}"; do
 done
 
 # Switch to orphan branch to build container image
-git checkout $current_branch
+git checkout -f $current_branch
